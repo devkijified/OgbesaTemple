f90bb0c83349f71afdbfbbee2af8e55bb90c7dbb
Boluwatife Olaniyan <42306524+devkijified@users.noreply.github.com>
2026-05-05 10:37:36 +0100

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient as createClientPrimitive } from '@supabase/supabase-js'
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const createClient = () => createClientPrimitive(supabaseUrl, supabaseAnonKey)
